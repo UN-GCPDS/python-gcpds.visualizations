23afc4ca9e16af37796d0b2a5d6f307155b4fb19
Yeison Cardona <yencardonaal@unal.edu.co>
2022-08-29 17:59:27 -0500

    Update project

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='gcpds-visualizations',
-    version='0.1a0',
+    version='0.1',
     packages=['gcpds.visualizations'],
 
     author='Yeison Cardona',
