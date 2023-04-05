0243ee94de3fbd1e86360981f208040b794e7d96
Yeison Cardona <yencardonaal@unal.edu.co>
2023-04-04 21:02:04 -0500

    Added topoplots

--- gcpds/visualizations/topoplots/__init__.py
+++ gcpds/visualizations/topoplots/__init__.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 
 
 # ----------------------------------------------------------------------
-def topoplot(data, channels, montage_name='standard_1020', cmap='coolwarm', resolution=64, interp='cubic', contours=0, ax=None, vmin=None, vmax=None):
+def topoplot(data, channels, montage_name='standard_1020', ax=None, **kwargs):
     """"""
     info = mne.create_info(
         channels,
@@ -20,17 +20,7 @@ def topoplot(data, channels, montage_name='standard_1020', cmap='coolwarm', reso
     mne.viz.plot_topomap(data,
                          info,
                          axes=ax,
-                         names=channels,
-                         sensors=False,
-                         # show_names=True,
-                         contours=contours,
-                         cmap=cmap,
-                         outlines='head',
-                         res=resolution,
-                         extrapolate='head',
-                         show=False,
-                         image_interp=interp,
-                         vlim=(vmin, vmax),
+                         **kwargs
                          )
 
     # plt.close()
